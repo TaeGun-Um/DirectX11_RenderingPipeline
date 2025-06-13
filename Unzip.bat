cfb864bc4a25fbf427446870f00b2a958c027985
엄태건 <umtimos@naver.com>
2025-06-13 20:08:23 +0900

    배치파일 수정

--- Unzip.bat
+++ Unzip.bat
@@ -8,3 +8,7 @@ IF NOT EXIST "%~dp0%ThirdParty\DirectXTex\lib\x64\Release\DirectXTex.lib" (
 IF NOT EXIST "%~dp0%ThirdParty\Assimp\lib\x64\Release\assimp-vc143-mt.lib" (
     powershell expand-archive ThirdParty\Assimp\lib.zip ThirdParty\Assimp\lib\
 )
+
+IF NOT EXIST "%~dp0%ThirdParty\DirectXTK\lib\x64\Release\DirectXTK.lib" (
+    powershell expand-archive ThirdParty\DirectXTK\lib.zip ThirdParty\DirectXTK\lib\
+)
